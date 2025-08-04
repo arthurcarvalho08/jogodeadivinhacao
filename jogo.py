8ac8a7f37a458908343cbf2b82ec0fa8988a2d01
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2025-08-04 14:07:36 +0000

    aula 4

--- jogo.py
+++ jogo.py
@@ -39,3 +39,30 @@ elif nivel == 2:
 elif nivel == 3:
     totalTentativas = 5
     print("5 TENTATIVAS????? VOCÊ É LOUCO OU CORAJOSO??????")
+
+
+for rodada in range (1, totalTentativas +1):
+    print("Tentativa {} de {}".format(rodada, totalTentativas))
+    chute_str = input("Digite um número entre 1 d 100: ")
+    chute = int(chute_str)
+
+    if(chute < 1 or > 100):
+        print("Número inválido.")
+        continue
+
+    acertou = chute == numeroSecreto 
+    maior = chute > numeroSecreto
+    menor = chute < numeroSecreto
+
+    if(acertou):
+        print(f"Você acertou e fez {pontos}!")
+        break
+    else:
+        if(maior):
+            print("Você errou! Seu chute foi maior que o número secreto.")
+        elif(menor):
+            print("Você errou! Seu chute foi menor que o número secreto.")
+
+            pontosPerdidos = abs(numeroSecreto - chute)
+            pontos = pontos - pontosPerdidos
+print("Fim de jogo! O número era ",numeroSecreto)
